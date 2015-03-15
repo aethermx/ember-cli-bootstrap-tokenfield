4cde21978a8c4206c33c3eb128f545c668f04f9b
Gastón I. Silva E <silvag777@gmail.com>
2015-03-14 23:35:19 -0600

    add blueprint

---
+++ blueprints/ember-cli-bootstrap-tokenfield/index.js
@@ -0,0 +1,28 @@
+module.exports = {
+  description: '',
+
+  // locals: function(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  normalizeEntityName: function() {
+    // allows us to run ember -g ember-cli-bootswatch and not blow up
+    // because ember cli normally expects the format
+    // ember generate <entitiyName> <blueprint>
+  },
+
+  afterInstall: function(options) {
+    var _this = this;
+    
+    console.log('Note: bootstrap-tokenfield depends on jquery 2.1.0');
+    console.log('      Use that.');
+    return _this.addBowerPackageToProject('jquery', '2.1.0').then(function() {
+      return _this.addBowerPackageToProject('jquery-ui', '1.11.4').then(function() {
+        return _this.addBowerPackageToProject('bootstrap-tokenfield');
+      });
+    });
+  }
+};
